ad3c2314864763de6fe08fc4c28e59242dc17665
heyho00 <heyho9292@gmail.com>
2023-03-09 23:44:38 +0900

    ✨ 관심사의 분리

--- src/components/Counter.tsx
+++ src/components/Counter.tsx
@@ -1,14 +1,23 @@
 import useForceUpdate from "../hooks/useForceUpdate";
 
+// Business logic
+
 const state = {
   count: 0,
 };
+
+function increase() {
+  state.count += Math.ceil(Math.random() * 10);
+}
+
+// UI
+// 이런 식으로 business logic과 ui를 나눈다. 관심사의 분리.
 
 export default function Counter() {
   const forceUpdate = useForceUpdate();
 
   const handleClick = () => {
-    state.count += 1;
+    increase();
     forceUpdate();
   };
 
--- src/hooks/useForceUpdate.ts
+++ src/hooks/useForceUpdate.ts
@@ -1,17 +1,7 @@
-import { useState } from "react";
+import { useCallback, useState } from "react";
 
 export default function useForceUpdate() {
-  const [state, setState] = useState({});
-
-  const forceUpdate = () => {
-    // setState(state);
-    // state+=1 을 그냥 state로 수정하면 바뀌지 않아 리렌더되지 않는다.
-    // 사실 이 훅은 리렌더만 시키는게 목적인 forceUpdate이기 때문에
-    // 값을 변화시킬 필요없이 빈 객체를 주고
-    // 스프레드로 복사본을 setState만 해주면 된다.(리렌더 됨)
-    // Counter.tsx 파일의 state 값의 변화로 인해 눈에 보이는 값은 올라간다.
-    // {...state} 안하고 {}만 해도 되네 ; 새객체구나.
-    setState({});
-  };
-  return forceUpdate;
+  const [, setState] = useState({});
+  return useCallback(() => setState({}), []);
+  // useCallback을 써서 함수도 바뀌지 않도록 해준다.
 }
