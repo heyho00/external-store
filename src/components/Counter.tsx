935d036b2e6e391429059ed1ea4d90dfa4cf8026
heyho00 <heyho9292@gmail.com>
2023-03-09 23:36:23 +0900

    ✨ useForceUpdate refac

--- src/components/Counter.tsx
+++ src/components/Counter.tsx
@@ -1,25 +1,23 @@
 import useForceUpdate from "../hooks/useForceUpdate";
 
-let count = 0;
+const state = {
+  count: 0,
+};
 
 export default function Counter() {
   const forceUpdate = useForceUpdate();
 
   const handleClick = () => {
-    count += 1;
+    state.count += 1;
     forceUpdate();
   };
 
   return (
     <div>
-      <p>{count}</p>
+      <p>{state.count}</p>
       <button type="button" onClick={handleClick}>
         Increase
       </button>
     </div>
   );
 }
-
-//count를 리액트 바깥에 두고 +=1 해도 리렌더가 되지 않는다.
-// state가 아니기 때문이다.
-// useForceUpdate 훅을 만들어 forceUpdate를 해준다.
